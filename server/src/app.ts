26e53169838af955b063291c044c523e83bcf840
Godswill Utong <godswillutong@gmail.com>
2024-03-15 10:07:46 +0100

    Port updated

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 const app = express();
-const port = config.get('PORT');
+const port = config.get('PORT') || 4000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
